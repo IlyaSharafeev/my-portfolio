556b457fdbf9ff4f02c8948dfae1f11bd365f060
IlyaSharafeev <ilyasharafeev01@gmail.com>
2023-04-26 19:15:21 +0300

    fix deployment (add npm vercel)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import vue from "@astrojs/vue";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), vue(), tailwind()],
-    build: {
-        transpile: ['maz-ui'],
-    },
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), vue(), tailwind()],
+  build: {
+    transpile: ['maz-ui']
+  },
+  output: "server",
+  adapter: vercel()
 });
