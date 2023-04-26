916b43b15ec231072c50d17450609b5e85cff07c
IlyaSharafeev <ilyasharafeev01@gmail.com>
2023-04-26 19:31:45 +0300

    add integrations @astro/image (add one image)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), vue(), tailwind()],
+  integrations: [mdx(), sitemap(), vue(), tailwind(), image()],
   build: {
     transpile: ['maz-ui']
   },
