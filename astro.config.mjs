cac336fb5d81af874314940ff94d1a035bb5cb49
IlyaSharafeev <ilyasharafeev01@gmail.com>
2023-04-27 14:08:04 +0300

    remove vercel serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,5 +12,4 @@ export default defineConfig({
     transpile: ['maz-ui']
   },
   output: "server",
-  adapter: vercel()
 });
